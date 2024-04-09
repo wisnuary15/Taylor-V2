d939b517449924a27b68c53ff167e3b41057caa3
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-04-10 00:56:03 +0800

    Add files via upload

--- plugins/Group/group-leavegc.js
+++ plugins/Group/group-leavegc.js
@@ -12,60 +12,60 @@ let handler = async (m, {
         .filter((key) => key.endsWith('@g.us'))
         .map((key) => conn.chats[key]);
 
-    if (args[0] && (args[0].toLowerCase() === 'all' || args[0].toLowerCase() === 'semua')) {
-        await Promise.all(chat.map(async (id) => {
+    args[0] && (args[0].toLowerCase() === 'all' || args[0].toLowerCase() === 'semua')
+        ? await Promise.all(chat.map(async (id) => {
             await conn.groupLeave(id);
             await delay(2000);
-        }));
-        let pesan = '*Success!* Semua grup telah di-leave.';
-        await m.reply(pesan);
-    } else {
-        try {
-            if (!args[0] || isNaN(args[0])) {
-                let usageMessage = `⚠️ *Invalid input.* Harap berikan nomor grup yang valid.\n\nCara Penggunaan: ${usedPrefix + command} <nomor grup>`;
-                let listMessage = "*List Grup:*\n" + groups.map((group, index) => `*${index + 1}.* ${group.subject}`).join('\n');
-                return m.reply(usageMessage + "\n\n" + listMessage);
-            }
+        })).then(() => m.reply('*Success!* Semua grup telah di-leave.'))
+        : (() => {
+            try {
+                if (!args[0] || isNaN(args[0])) {
+                    let usageMessage = `⚠️ *Invalid input.* Harap berikan nomor grup yang valid.\n\nCara Penggunaan: ${usedPrefix + command} <nomor grup>`;
+                    let listMessage = "*List Grup:*\n" + groups.map((group, index) => `*${index + 1}.* ${group.subject}`).join('\n');
+                    return m.reply(usageMessage + "\n\n" + listMessage);
+                }
 
-            let i = parseInt(args[0]);
-            if (i <= 0 || i > groups.length) {
-                let listMessage = "*⚠️ Invalid input.* Harap gunakan nomor grup yang valid.\n\n" + groups.map((group, index) => `*${index + 1}.* ${group.subject}`).join('\n');
-                return m.reply(listMessage);
-            }
+                let i = parseInt(args[0]);
+                if (i <= 0 || i > groups.length) {
+                    let listMessage = "*⚠️ Invalid input.* Harap gunakan nomor grup yang valid.\n\n" + groups.map((group, index) => `*${index + 1}.* ${group.subject}`).join('\n');
+                    return m.reply(listMessage);
+                }
 
-            let groupIndex = i - 1;
+                let groupIndex = i - 1;
 
-            let str = `${[i]}
-*Nama Grup :* ${groups[groupIndex].subject} 
-*Owner :* ${groups[groupIndex].owner ? "@" + groups[groupIndex].owner.split("@")[0] : "Tidak Diketahui"}
-*Subject Owner :* ${groups[groupIndex].subjectOwner ? "@" + groups[groupIndex].subjectOwner.split("@")[0] : "Tidak Diketahui"}
-*ID :* ${groups[groupIndex].id}
-*Restrict :* ${groups[groupIndex].restrict}
-*Announce :* ${groups[groupIndex].announce}
-*Ephemeral :* ${new Date(groups[groupIndex].ephemeralDuration * 1000).toDateString()}
-*Desc ID :* ${groups[groupIndex].descId}
-*Description :* ${groups[groupIndex].desc?.toString().slice(0, 10) + '...' || 'Tidak Diketahui'}
-*Admins :* ${groups[groupIndex].participants.filter(p => p.admin).map((v, i) => `\n${i + 1}. @${v.id.split('@')[0]}`).join(' [admin]')}
-${isOwner ? `*Participants :* ${groups[groupIndex].participants.length}` : ''}
-${isOwner ? `*isBotAdmin :* [ ${!!groups[groupIndex].participants.find(v => v.id == conn.user.jid).admin} ]` : ''}
-*Created :* ${new Date(groups[groupIndex].subjectTime * 1000).toDateString()}
-*Creation :* ${new Date(groups[groupIndex].creation * 1000).toDateString()}
-*Size :* ${groups[groupIndex].size}
+                let str = `${[i]}
+*Nama Grup :* ${groups[groupIndex].metadata.subject} 
+*Owner :* ${groups[groupIndex].metadata.owner ? "@" + groups[groupIndex].metadata.owner.split("@")[0] : "Tidak Diketahui"}
+*Subject Owner :* ${groups[groupIndex].metadata.subjectOwner ? "@" + groups[groupIndex].metadata.subjectOwner.split("@")[0] : "Tidak Diketahui"}
+*ID :* ${groups[groupIndex].metadata.id}
+*Restrict :* ${groups[groupIndex].metadata.restrict}
+*Announce :* ${groups[groupIndex].metadata.announce}
+*Ephemeral :* ${new Date(groups[groupIndex].metadata.ephemeralDuration * 1000).toDateString()}
+*Desc ID :* ${groups[groupIndex].metadata.descId}
+*Description :* ${groups[groupIndex].metadata.desc?.toString().slice(0, 10) + '...' || 'Tidak Diketahui'}
+*Admins :* ${groups[groupIndex].metadata.participants.filter(p => p.admin).map((v, i) => `\n${i + 1}. @${v.id.split('@')[0]}`).join(' [admin]')}
+${isOwner ? `*Participants :* ${groups[groupIndex].metadata.participants.length}` : ''}
+${isOwner ? `*isBotAdmin :* [ ${!!groups[groupIndex].metadata.participants.find(v => v.id == conn.user.jid).admin} ]` : ''}
+*Created :* ${new Date(groups[groupIndex].metadata.subjectTime * 1000).toDateString()}
+*Creation :* ${new Date(groups[groupIndex].metadata.creation * 1000).toDateString()}
+*Size :* ${groups[groupIndex].metadata.size}
 `;
 
-            await m.reply(str);
-            await conn.groupLeave(groups[groupIndex].id);
-            await delay(2000);
-            await m.reply('*Success!* Grup ' + groups[groupIndex].subject + ' telah di-leave!');
-        } catch (err) {
-            await m.reply('*Error!* Terjadi kesalahan.');
-            console.error(err);
-        }
-    }
+                return m.reply(str)
+                    .then(() => conn.groupLeave(groups[groupIndex].metadata.id))
+                    .then(() => delay(2000))
+                    .then(() => m.reply('*Success!* Grup ' + groups[groupIndex].metadata.subject + ' telah di-leave!'));
+            } catch (err) {
+                return m.reply('*Error!* Terjadi kesalahan.')
+                    .then(() => console.error(err));
+            }
+        })();
 };
 
 handler.help = ['leave', 'leavegc'].map((v) => v + ' <nomor grup>');
 handler.tags = ['group'];
 handler.command = /^leave(g(c|ro?up))?$/i;
 handler.rowner = true;
+handler.owner = true;
+handler.premium = true;
 export default handler;
