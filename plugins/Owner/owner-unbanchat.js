02538c279f03e372837e8a10ac73a85aa18daa47
AyGemuy <abdmalikalqadri2001@gmail.com>
2024-06-05 10:42:08 +0000

    Taylor-V2

--- plugins/Owner/owner-unbanchat.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m) => {
-  db.data.chats[m.chat].isBanned = false
-  m.reply('Done!')
-}
-handler.help = ['unbanchat']
-handler.tags = ['owner']
-handler.command = /^unbanchat$/i
-handler.owner = true
-export default handler
