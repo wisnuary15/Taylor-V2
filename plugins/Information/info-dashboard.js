9a790f5fbdcadaa302895d45da8dbd850585dde0
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-06-18 12:53:45 +0800

    Add files via upload

--- plugins/Information/info-dashboard.js
+++ plugins/Information/info-dashboard.js
@@ -1,31 +1,34 @@
 const handler = async (m, { conn: conn }) => {
-  let stats = Object.entries(db.data.stats).map((([key, val]) => {
-    let name = Array.isArray(plugins[key].help) ? plugins[key].help?.join("\n• ") : plugins[key].help || key;
-    if (!/exec/.test(name)) return {
-      name: name,
-      ...val
+  try {
+    const sortedData = Object.entries(db.data?.stats).sort(([, a], [, b]) => b.total - a.total).slice(0, 10);
+
+    const formatNumber = (num) => {
+      const suffixes = ['', ' ribu', ' juta', ' miliar', ' triliun'];
+      let suffixIndex = 0;
+      while (num >= 1000 && suffixIndex < suffixes.length - 1) num /= 1000, suffixIndex++;
+      return `${Math.round(num)}${suffixes[suffixIndex]}`;
     };
-  }));
-  stats = stats.sort(((a, b) => b.total - a.total));
-  let txt = stats.slice(0, 10).map((({ name: name, total: total, last: last }, idx) => (name.includes("-") && name.endsWith(".js") && (name = name.split("-")[1].replace(".js", "")),
-    `${htki} ${idx + 1} ${htka}\n*${htjava} C M D ${htjava}*\n${name}\n\n*${htjava} H I T ${htjava}*\n${total}\n\n*${htjava} T I M E ${htjava}*\n${getTime(last)}\n`))).join`\n\n`;
-  await conn.reply(m.chat, txt, m);
+
+    const formatTime = (time) => {
+      const timeDiff = Math.floor((Date.now() - time) / 1000);
+      const intervals = { tahun: 31536000, bulan: 2592000, minggu: 604800, hari: 86400, jam: 3600, menit: 60, detik: 1 };
+
+      for (const [unit, seconds] of Object.entries(intervals)) if (Math.floor(timeDiff / seconds) > 0) return `${Math.floor(timeDiff / seconds)} ${unit} yang lalu`;
+
+      return 'Baru saja';
+    };
+
+    const output = sortedData.map(([name, { total, success, last, lastSuccess }], idx) =>
+      `*${idx + 1}). ${name.split('/').pop().replace('.js', '')}*\n   - Total: \`${formatNumber(total)}\`\n   - Success: \`${formatNumber(success)}\`\n   - Last: \`${formatTime(last)}\`\n   - Last Success: \`${formatTime(lastSuccess)}\``).join('\n\n');
+    await conn.reply(m.chat, output, m);
+  } catch (error) {
+    console.error('Error:', error);
+    await conn.reply(m.chat, 'Terjadi kesalahan dalam memproses permintaan.', m);
+  }
 };
-handler.help = ["dashboard"], handler.tags = ["info"], handler.command = /^^d(as(hbo(ard?|r)|bo(ard?|r))|b)$/i;
+
+handler.help = ["dashboard"];
+handler.tags = ["info"];
+handler.command = /^d(as(hbo(ard?|r)|bo(ard?|r))|b)$/i;
+
 export default handler;
-export function parseMs(ms) {
-  if ("number" != typeof ms) throw "Parameter must be filled with number";
-  return {
-    days: Math.trunc(ms / 864e5),
-    hours: Math.trunc(ms / 36e5) % 24,
-    minutes: Math.trunc(ms / 6e4) % 60,
-    seconds: Math.trunc(ms / 1e3) % 60,
-    milliseconds: Math.trunc(ms) % 1e3,
-    microseconds: Math.trunc(1e3 * ms) % 1e3,
-    nanoseconds: Math.trunc(1e6 * ms) % 1e3
-  };
-}
-export function getTime(ms) {
-  let now = parseMs(+new Date - ms);
-  return now.days ? `${now.days} Hari yang lalu` : now.hours ? `${now.hours} Jam yang lalu` : now.minutes ? `${now.minutes} Menit yang lalu` : "Barusan";
-}
