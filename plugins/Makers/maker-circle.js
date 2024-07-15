88883cf4ad0b7ad104a1dc8ad53c59e358bc07b6
AyGemuy <abdmalikalqadri2001@gmail.com>
2024-07-15 06:01:02 +0000

    Taylor-V2

---
+++ plugins/Makers/maker-circle.js
@@ -0,0 +1,16 @@
+const handler = async (m, {
+  conn,
+  args,
+  usedPrefix,
+  command
+}) => {
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender,
+    url = await conn.profilePictureUrl(who).catch(_ => "./src/avatar_contact.png"),
+    scircle = API("dzx", "/api/canvas/circle", {
+      url: url
+    });
+  await conn.sendFile(m.chat, scircle, "pp.jpg", "*Done!*", m);
+};
+handler.help = ["circle"].map(v => v + " <tag>"), handler.tags = ["maker"],
+  handler.command = /^(cir(cle)?(le)?(cele)?(kel)?)$/i;
+export default handler;
