122ee4cfedda8a1c02fd4da0c50754b4d0be07ca
AyGemuy <abdmalikalqadri2001@gmail.com>
2024-07-24 12:51:29 +0700

    Taylor-V2

--- plugins/AI/ai-faceswap.js
+++ plugins/AI/ai-faceswap.js
@@ -1,34 +1,39 @@
 import {
   Prodia
 } from "prodia.js";
+import fetch from "node-fetch";
 import _ from "lodash";
 import uploadFile from "../../lib/uploadFile.js";
 import uploadImage from "../../lib/uploadImage.js";
 const prodiaClient = Prodia(_.sample([...prodiaKey]));
+const isUrl = text => text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|png)/, "gi"));
 const handler = async (m, {
-  conn
+  conn,
+  text,
+  usedPrefix,
+  command
 }) => {
   try {
     const q = m.quoted || m;
+    if (!text || !isUrl(text)) {
+      return m.reply(`❓ *Query input tidak valid!*\n\nPastikan input adalah link gambar.\nContoh penggunaan:\n${usedPrefix + command} [link gambar]`);
+    }
+    const prompt = text.trim();
     const mime = (q.msg || q).mimetype || "";
     if (!mime) return m.reply("Tidak ada media yang ditemukan");
     const media = await q.download();
     const link = await (/image\/(png|jpe?g|gif)|video\/mp4/.test(mime) ? uploadImage : uploadFile)(media);
     const generateImageParams = {
-      imageUrl: link,
-      imageData: media.toString("base64")
+      sourceUrl: link,
+      targetUrl: prompt
     };
-    if (!generateImageParams.imageUrl || !generateImageParams.imageData) {
-      return m.react(eror);
-    }
-    m.react(wait);
     const result = await generateImage(generateImageParams);
     if (result) {
       await conn.sendMessage(m.chat, {
         image: {
           url: result?.imageUrl || thumb
         },
-        caption: `✨ *${command?.toUpperCase()}*\n- *Request oleh:* @${m.sender.split("@")[0]}`,
+        caption: `✨ *Gambar berhasil dibuat!*\n- Link: *${link}*\n- *Request oleh:* @${m.sender.split("@")[0]}\n- *Prompt:* ${prompt}`,
         mentions: [m.sender]
       }, {
         quoted: m
@@ -42,13 +47,13 @@ const handler = async (m, {
     m.react(eror);
   }
 };
-handler.help = ["facerestore *[query]* --option"];
+handler.help = ["faceswap *[link gambar]*"];
 handler.tags = ["ai"];
-handler.command = /^(facerestore)$/i;
+handler.command = /^(faceswap)$/i;
 export default handler;
 async function generateImage(params) {
   try {
-    const generate = await prodiaClient.faceRestore(params);
+    const generate = await prodiaClient.faceSwap(params);
     return await prodiaClient.wait(generate) || null;
   } catch (error) {
     console.error("Error generating image:", error);
