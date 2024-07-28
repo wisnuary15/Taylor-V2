273d6a3b829a411e0fa46a3ad40264c611be5525
AyGemuy <abdmalikalqadri2001@gmail.com>
2024-07-28 17:16:44 +0000

    Taylor-V2

---
+++ plugins/Tools/tools-webcek.js
@@ -0,0 +1,33 @@
+import https from "https";
+import fetch from "node-fetch";
+const handler = async (m, {
+  args,
+  usedPrefix,
+  command
+}) => {
+  try {
+    if (!args[0]) throw `Contoh: ${usedPrefix + command} nekopoi.care`;
+    const message = (await checkWeb(args[0])).map(v => `🌐 *Domain:* ${v.Domain}\n🛡️ *Status:* ${v.Status}`).join("\n\n");
+    m.reply(message);
+  } catch (error) {
+    m.reply(error);
+  }
+};
+handler.command = /^web(check|cek)|(check|cek)web$/i;
+export default handler;
+async function checkWeb(url) {
+  try {
+    return (await (await fetch("https://trustpositif.kominfo.go.id/Rest_server/getrecordsname_home", {
+      agent: new https.Agent({
+        rejectUnauthorized: true,
+        agent: "http://proxy:clph123@103.123.63.106:3128"
+      }),
+      method: "post",
+      body: new URLSearchParams({
+        name: url
+      })
+    })).json()).values;
+  } catch (error) {
+    throw "Terjadi kesalahan saat memeriksa status website";
+  }
+}
